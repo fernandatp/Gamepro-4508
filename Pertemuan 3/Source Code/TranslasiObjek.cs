ed942c576feae6916fb5be23f8e55bb8eb28c2e8
Fernanda <97970521+fernandatp@users.noreply.github.com>
2022-11-12 11:41:22 +0700

    Add files via upload

---
+++ Pertemuan 3/Source Code/TranslasiObjek.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 kekanan;
+    Vector3 kekiri;
+    Vector3 keatas;
+    Vector3 kebawah;
+    void Start()
+    {
+        kekanan = new Vector3 [1,0,0];
+        kekiri = new Vector3 [-1,0,0];
+        keatas = new Vector3 [0,1,0];
+        kebawah = new Vector3 [0,-1,0];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (kekanan * 2f * Time.deltaTime);
+        
+    }
+}
